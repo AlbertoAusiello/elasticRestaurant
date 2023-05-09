eb6793d5a369c03d98b85f3268ce2b710587d6c7
AlbertoAusiello <ausielloalberto01@gmail.com>
2023-05-09 16:06:13 +0200

    entities

---
+++ src/main/java/it/sysman/elasticRestaurant/model/Address.java
@@ -0,0 +1,94 @@
+package it.sysman.elasticRestaurant.model;
+
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Field;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+@Setter
+@Getter
+@NoArgsConstructor
+@ToString
+public class Address {
+	@Id
+	private Long id;
+	
+	@Field
+	@JsonProperty("amenity")
+	private String amenity;
+	
+	@Field
+	@JsonProperty("road")
+	private String road;
+	
+	@Field
+	@JsonProperty("house_number")
+	private String houseNumber;
+	
+	@Field
+	@JsonProperty("neighbourhood")
+	private String neighbourhood;
+	
+	@Field
+	@JsonProperty("suburb")
+	private String suburb;
+	
+	@Field
+	@JsonProperty("city")
+	private String city;
+	
+	@Field
+	@JsonProperty("borough")
+	private String borough;
+	
+	@Field
+	@JsonProperty("ISO3166-2-lvl4")
+	private String ISO3166_14;
+	
+	@Field
+	@JsonProperty("ISO3166-2-lvl6")
+	private String ISO3166_16;
+	
+	@Field
+	@JsonProperty("postcode")
+	private String postCode;
+	
+	@Field
+	@JsonProperty("region")
+	private String region;
+	
+	@Field
+	@JsonProperty("county")
+	private String county;
+	
+	@Field
+	@JsonProperty("state")
+	private String state;
+	
+	@Field
+	@JsonProperty("country")
+	private String country;
+	
+	@Field
+	@JsonProperty("country_code")
+	private String countryCode;
+	
+	@Field
+	@JsonProperty("municipality")
+	private String municipality;
+	
+	@Field
+	@JsonProperty("town")
+	private String town;
+	
+	@Field
+	@JsonProperty("city_district")
+	private String cityDistrict;
+	
+	@Field
+	@JsonProperty("quarter")
+	private String quarter;
+}
---
+++ src/main/java/it/sysman/elasticRestaurant/model/Restaurant.java
@@ -0,0 +1,66 @@
+package it.sysman.elasticRestaurant.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Field;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+@Setter
+@Getter
+@NoArgsConstructor
+@ToString
+public class Restaurant {
+	@Id
+	private Long id;
+	
+	@Field
+	@JsonProperty("place_id")
+	private Long placeId;
+	
+	@Field
+	@JsonProperty("osm_id")
+	private Long osmId;
+	
+	@Field
+	@JsonProperty("licence")
+	private String licence;
+	
+	@Field
+	@JsonProperty("osm_type")
+	private String osmType;
+	
+	@Field
+	@JsonProperty("lat")
+	private String lat;
+	
+	@Field
+	@JsonProperty("lon")
+	private String lon;
+	
+	@Field
+	@JsonProperty("display_name")
+	private String displayName;
+	
+	@Field
+	@JsonProperty("class")
+	private String class1;
+	
+	@JsonProperty("type")
+	private String type;
+	
+	@Field
+	@JsonProperty("icon")
+	private String icon;
+	
+	@Field
+	@JsonProperty("importance")
+	private double importance;
+	
+	@Field
+	@JsonProperty("address")
+	private Address address=new Address();
+	
+	
+}
