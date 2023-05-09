7fcd9145a817f69e67c0931ed67607e55714f331
AlbertoAusiello <ausielloalberto01@gmail.com>
2023-05-09 12:24:02 +0200

    Initial commit

---
+++ src/main/java/it/sysman/elasticRestaurant/ElasticRestaurantApplication.java
@@ -0,0 +1,13 @@
+package it.sysman.elasticRestaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticRestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticRestaurantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/sysman/elasticRestaurant/ElasticRestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package it.sysman.elasticRestaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticRestaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
